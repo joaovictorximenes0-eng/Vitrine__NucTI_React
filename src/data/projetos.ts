23e0706f90144dc60444748ad3a92c9bf8f867b0
erickalvesflk <erickalvesflk@gmail.com>
2026-09-14 00:57:34 -0300

    Cores dinâmicas para projetos

--- src/data/projetos.ts
+++ src/data/projetos.ts
@@ -1,12 +1,31 @@
 import type { Projeto, ProjetoSemId } from '../types/Nucti';
 import { addIdProject } from './uuid5'
 
+const projects_colors = [
+  '#0f172a',
+  '#1e293b',
+  '#293548',
+  '#334155',
+  '#3d4d61',
+  '#475569',
+  '#52647a',
+  '#64748b',
+];
+function get_project_color(nome: string): string{
+  const relative_index = (n: number, l: number): number => n%l===0 ? n%l : (n%l)-1;
+
+  const l_formated = nome.replaceAll(' ','').toLowerCase()
+  const choiced_tone = projects_colors[relative_index(l_formated.charCodeAt(1),projects_colors.length)]
+
+  return choiced_tone.replaceAll("#",'')
+}
+
 const projetos_sem_id: ProjetoSemId[] = [
   {
     titulo: "Sistemas Embarcados e IoT",
     resumo: "Monitoramento ambiental e automação de salas usando ESP32 e redes de sensores.",
     descricaoCompleta: "...",
-    fotos: ["https://placehold.co/800x500/1e293b/ffffff?text=Bancada+IoT"],
+    fotos: [`https://placehold.co/800x500/${get_project_color('SE')}/ffffff?text=Bancada+IoT`],
     formLink: "https://forms.google.com",
     membrosIds: [
       "4df90e4fb61f5a08a6a8e8c8678e0a8e", 
@@ -18,7 +37,7 @@ const projetos_sem_id: ProjetoSemId[] = [
     titulo: "Aplicativo de Gestão Acadêmica",
     resumo: "Plataforma mobile para alunos acompanharem horários, eventos e avisos do núcleo.",
     descricaoCompleta: "...",
-    fotos: ["https://placehold.co/800x500/334155/ffffff?text=Interface+Mobile"],
+    fotos: [`https://placehold.co/800x500/${get_project_color('IM')}/ffffff?text=Interface+Mobile`],
     formLink: "https://forms.google.com",
     membrosIds: [
       "2e5c718c5e775e2784217806e9018a5a", 
@@ -29,7 +48,7 @@ const projetos_sem_id: ProjetoSemId[] = [
     titulo: "Visão Computacional e IA",
     resumo: "Modelos de IA para contagem de fluxo de pessoas e reconhecimento de padrões.",
     descricaoCompleta: "...",
-    fotos: ["https://placehold.co/800x500/475569/ffffff?text=Visão+Computacional+e+IA"],
+    fotos: [`https://placehold.co/800x500/${get_project_color('VC')}/ffffff?text=Visão+Computacional+e+IA"`],
     formLink: "https://forms.google.com",
     membrosIds: [
       "4df90e4fb61f5a08a6a8e8c8678e0a8e",
@@ -42,7 +61,7 @@ const projetos_sem_id: ProjetoSemId[] = [
     titulo: "CompetiTech",
     resumo: "Ensino de Robótica com Robôs de Competição",
     descricaoCompleta: "Em desenvolvimento...",
-    fotos: ["https://placehold.co/800x500/475569/ffffff?text=CompetiTech"],
+    fotos: [`https://placehold.co/800x500/${get_project_color('CT')}/ffffff?text=CompetiTech`],
     formLink: "https://forms.google.com",
     membrosIds: [
       "0f37555a5eb55c8e85a12465e2f445c1"
@@ -52,7 +71,7 @@ const projetos_sem_id: ProjetoSemId[] = [
     titulo: "Explorando Saber",
     resumo: "desenvolvimento de plataforma de ensino para alunos do Ensino Médio",
     descricaoCompleta: "...",
-    fotos: ["https://placehold.co/800x500/475569/ffffff?text=Explorando+Saber"],
+    fotos: [`https://placehold.co/800x500/${get_project_color('ES')}/ffffff?text=Explorando+Saber`],
     formLink: "https://forms.google.com",
     membrosIds: [
       "e037a53172985e4b8a2a5c82c0000691", 
